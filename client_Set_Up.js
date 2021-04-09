5e57e860dc417c16f080d76c54cc14413d74149a
Yaacov <yaacov.royde@haffnerhoff.co.uk>
2021-04-09 17:17:19 +0100

    initial display styling

--- client_Set_Up.js
+++ client_Set_Up.js
@@ -1,17 +1,11 @@
-const clients = [1];
-
-document.getElementById('clientSetUp').onsubmit = (e) => {
+document.getElementById('clientSetUpForm').onsubmit = (e) => {
     e.preventDefault();
     const client = {
         name: document.getElementById('clientName').value
     };
-    clients.push(client);
-    console.log(clients);
-    clients.forEach(client => {
-        const clientOption = document.createElement('option');
-        clientOption.innerHTML = client.name;
-        clientOption.value = client.name;
-        document.getElementById('clients').appendChild(clientOption);
-    });
+    const clientOption = document.createElement('option');
+    clientOption.innerHTML = client.name;
+    clientOption.value = client.name;
+    document.getElementById('clientsList').appendChild(clientOption);
 };
 
--- member_Set_Up.js
+++ member_Set_Up.js
@@ -1,17 +1,21 @@
-const members = [];
+document.getElementById('memberSetUpLink').onclick = () => {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('setUp').style.display = 'block';
+};
 
-document.getElementById('memberSetUp').onsubmit = (e) => {
+document.getElementById('homeLink').onclick = () => {
+    document.getElementById('setUp').style.display = 'none';
+    document.getElementById('home').style.display = 'block';    
+};
+
+document.getElementById('memberSetUpForm').onsubmit = (e) => {
     e.preventDefault();
     const member = {
         name: document.getElementById('memberName').value
     };
-    members.push(member);
-    console.log(members);
-    members.forEach(member => {
-        const memberOption = document.createElement('option');
-        memberOption.innerHTML = member.name;
-        memberOption.value = member.name;
-        document.getElementById('members').appendChild(memberOption);
-    });
+    const memberOption = document.createElement('option');
+    memberOption.innerHTML = member.name;
+    memberOption.value = member.name;
+    document.getElementById('membersList').appendChild(memberOption);
 };
 
